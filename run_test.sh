eae3483a42d4ceb5f78dba3cabac6b8614485d8d
n30nx <n30nxsec@protonmail.com>
2024-08-10 06:46:05 +0300

    fix: correct transcript in test

--- run_test.sh
+++ run_test.sh
@@ -2,6 +2,6 @@
 # cargo run --release --example create_transcript -- -n 2
 # cargo run --release --example create_helpers -- -n 2
 cargo run --release --example create_keys -- -n 2 -k 1
-cargo run --release -- --bls-key keys/1-bls --transcript transcript-2 --api-port 8080 --test &
+cargo run --release -- --bls-key keys/1-bls --transcript transcript-512 --api-port 8080 --test &
 cd tests
 go run main.go
